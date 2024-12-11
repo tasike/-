4db3b71a037c2db60caf38a58a6a774a7b3df45d
tasike <150120386+tasike@users.noreply.github.com>
2024-12-12 07:42:04 +0800

    Add files via upload

---
+++ 密码学实验四/Pollard's  𝑝 − 1 p−1 Algorithm Attack.py
@@ -0,0 +1,45 @@
+# Pollard p-1 算法攻击
+
+from gmpy2 import invert , powmod , is_prime , gcd , next_prime
+import binascii
+
+n = []
+e = []
+c = []
+ans = []
+
+def Pollard_p_1(N):
+    a = 2
+    f = a
+    while 1:
+        for n in range(1,200000):
+            f = powmod(f, n, N)
+            if is_prime(n):
+                d = gcd(f-1, N)
+                if 1 < d < N:
+                    return d
+                elif d >= N:
+                    f = next_prime(a)
+                    break
+        else:
+            break
+
+name = ['Frame2','Frame6','Frame19']
+
+for i in name:
+    f = open(i,'r')
+    data = f.read()
+    n0 , e0 , c0 = int(data[:256] , 16) , int(data[256:512] , 16) , int(data[512:] , 16)
+    n.append(n0)
+    e.append(e0)
+    c.append(c0)
+
+for i in range(len(n)):
+    p = Pollard_p_1(n[i])
+    q = n[i]//p
+    phi_of_frame = (p-1)*(q-1)
+    d = invert(e[i], phi_of_frame)
+    m = powmod(c[i], d, n[i])
+    ans.append(binascii.a2b_hex(hex(m)[-16:]))
+
+print(ans)
